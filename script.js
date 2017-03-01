f140c8135e0f0fe1cd88af70f52427812d69cfca
lucyrose93 <lucyrose@protonmail.com>
2017-03-01 17:06:57 +0000

    Build basic time counter
    Build a basic time counter, using the code solutions to basic tests.

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
-
-
-function secondsTimer(n){
-  if(n){
-    return n;
-  }
-  return 0;
-
-}
-
-var increment = 1000; //this is the increment between the "seconds hand" ticking (so to speak) in milliseconds
-
+var increment = 100; //this is the increment between the "seconds hand" ticking (so to speak) in milliseconds
 var clearSecondsAt = 60; //this is the amount of times the secondsTimer should increment before clearing and converting to minutes
+var timeCounter = 1;//The time displayed increases by the value of timeCounter after the increment has passed
+
+
+function secondsTimer(startTime){
+
+  var time;
+
+  if (startTime){
+    time = startTime;
+  }
+  else {
+    time = 0;
+  }
+
+
+  function secondsCounter(){
+    time += timeCounter;
+    console.log(time);
+    if (time === clearSecondsAt) {
+      clearInterval(interval);
+    }
+  }
+
+  var interval = setInterval(secondsCounter, increment);
+
+};
+
+secondsTimer();
