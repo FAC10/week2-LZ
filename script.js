3002690b8f0881c299e191699ed7e544310662cb
lucyrose93 <lucyrose@protonmail.com>
2017-03-02 12:40:55 +0000

    Create HTML boilerplate
    We created a basic interface so that we can start testing for DOM manipulation

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 
 var increment = 1000; //this is the increment between the "seconds hand" ticking (so to speak) in milliseconds
 var clearSecondsAt = 60; //this is the amount of times the secondsTimer should increment before clearing and converting to minutes
-var timeCounter = 1;//The time displayed increases by the value of timeCounter after the increment has passed
+var timeCounter = 1; //The time displayed increases by the value of timeCounter after the increment has passed
 
 
 //sets start time for seconds
-function timeAtStart(startTime){
-  if (startTime){
-    return startTime;
-  }
-  else {
-    return 0;
-  }
+function timeAtStart(startTime) {
+    if (startTime) {
+        return startTime;
+    } else {
+        return 0;
+    }
 }
 
 var time = timeAtStart();
 
-function secondsCounter(){
-  time += timeCounter;
-  console.log(time);
-  if (time === clearSecondsAt) {
-    clearInterval(interval);
-  }
+
+//counts seconds beginning at startTime
+
+function secondsCounter() {
+    time += timeCounter;
+    console.log(time);
+    if (time === clearSecondsAt) {
+        clearInterval(interval);
+    }
 }
 
 var interval;
 
-function secondsCounterRepeat(){
-  interval = setInterval(secondsCounter, increment);
+//Invokes secondsCounter to repeat after every increment (i.e. second) until the interval is cleared (after 60 secs)
+
+function secondsCounterRepeat() {
+    interval = setInterval(secondsCounter, increment);
 };
 
-// function testSecondsCounterRepeat(){
-//   console.log(time);
-//   return time;
-// }
-
 secondsCounterRepeat();
-// secondsCounter();
