99c93b23e77ea27e1ebdbf13cf1e92ff8962e9e4
Zooey <zooey@lbk.org.uk>
2017-03-02 12:14:33 +0000

    refactor function and change tests relates to #5

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 
-var increment = 100; //this is the increment between the "seconds hand" ticking (so to speak) in milliseconds
+var increment = 1000; //this is the increment between the "seconds hand" ticking (so to speak) in milliseconds
 var clearSecondsAt = 60; //this is the amount of times the secondsTimer should increment before clearing and converting to minutes
 var timeCounter = 1;//The time displayed increases by the value of timeCounter after the increment has passed
 
 
-function secondsTimer(startTime){
-
-  var time;
-
+//sets start time for seconds
+function timeAtStart(startTime){
   if (startTime){
-    time = startTime;
+    return startTime;
   }
   else {
-    time = 0;
+    return 0;
   }
+}
 
+var time = timeAtStart();
 
-  function secondsCounter(){
-    time += timeCounter;
-    console.log(time);
-    if (time === clearSecondsAt) {
-      clearInterval(interval);
-    }
+function secondsCounter(){
+  time += timeCounter;
+  console.log(time);
+  if (time === clearSecondsAt) {
+    clearInterval(interval);
   }
+}
 
-  var interval = setInterval(secondsCounter, increment);
+var interval;
 
+function secondsCounterRepeat(){
+  interval = setInterval(secondsCounter, increment);
 };
 
-secondsTimer();
+// function testSecondsCounterRepeat(){
+//   console.log(time);
+//   return time;
+// }
+
+secondsCounterRepeat();
+// secondsCounter();
